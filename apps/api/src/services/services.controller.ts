a84c78368693ffda945cd4a1980f3b885a7b57bb
yzy <yanziyi290@gmail.com>
2025-09-15 09:55:44 +0100

    中文改英文

--- apps/api/src/services/services.controller.ts
+++ apps/api/src/services/services.controller.ts
@@ -8,7 +8,7 @@ import { Role } from '@prisma/client';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { ApiCommonResponses } from 'src/common/decorators/api-common-responses.decorator';
-@ApiTags('服务管理')
+@ApiTags('Services management')
 @ApiBearerAuth() // 表明需要 Bearer Token 认证
 @Controller('services')
 export class ServicesController {
@@ -16,8 +16,8 @@ export class ServicesController {
 
   // --- 管理员权限 ---
   @Post()
-  @ApiOperation({ summary: '创建服务' })
-  @ApiResponse({ status: 201, description: '服务创建成功' })
+  @ApiOperation({ summary: 'Create service' })
+  @ApiResponse({ status: 201, description: 'Service created successfully' })
   @ApiCommonResponses()
   @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Roles(Role.ADMIN)
@@ -26,8 +26,8 @@ export class ServicesController {
   }
 
   @Patch(':id')
-  @ApiOperation({ summary: '更新服务信息' })
-  @ApiResponse({ status: 200, description: '服务信息更新成功' })
+  @ApiOperation({ summary: 'Update service information' })
+  @ApiResponse({ status: 200, description: 'Service information updated successfully' })
   @ApiCommonResponses()
   @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Roles(Role.ADMIN)
@@ -36,8 +36,8 @@ export class ServicesController {
   }
 
   @Delete(':id')
-  @ApiOperation({ summary: '删除服务' })
-  @ApiResponse({ status: 200, description: '服务删除成功' })
+  @ApiOperation({ summary: 'Delete service' })
+  @ApiResponse({ status: 200, description: 'Service deleted successfully' })
   @ApiCommonResponses()
   @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Roles(Role.ADMIN)
@@ -47,16 +47,16 @@ export class ServicesController {
 
   // --- 公开接口 ---
   @Get()
-  @ApiOperation({ summary: '获取所有服务信息' })
-  @ApiResponse({ status: 200, description: '返回所有服务信息' })
+  @ApiOperation({ summary: 'Get all services information' })
+  @ApiResponse({ status: 200, description: 'Return all services information' })
   @ApiCommonResponses()
   findAll() {
     return this.servicesService.findAll();
   }
 
   @Get(':id')
-  @ApiOperation({ summary: '获取单个服务信息' })
-  @ApiResponse({ status: 200, description: '返回服务信息' })
+  @ApiOperation({ summary: 'Get single service information' })
+  @ApiResponse({ status: 200, description: 'Return service information' })
   @ApiCommonResponses()
   findOne(@Param('id') id: string) {
     return this.servicesService.findOne(id);
