625ec999fa9b23f9b7bca6ea09407e2dee37eb0f
yzy <yanziyi290@gmail.com>
2025-09-15 09:56:12 +0100

    @ApiBearerAuth()误操作，移除这个

--- apps/api/src/payments/payments.controller.ts
+++ apps/api/src/payments/payments.controller.ts
@@ -1,11 +1,10 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { ApiCommonResponses } from 'src/common/decorators/api-common-responses.decorator';
 
 @ApiTags('Payments management')
-@ApiBearerAuth()
 @Controller('payments')
 @UseGuards(AuthGuard('jwt'))
 export class PaymentsController {
